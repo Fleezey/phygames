b7e361d137b3e0cdd1ac52d608360fa8a83357a2
Matthieu <matthieu@unityadmins-MacBook-Pro-2.local>
2020-02-13 17:36:17 -0500

    Init

---
+++ Assets/Scripts/LineTracer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineTracer : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("First commit!");
+    }
+}
