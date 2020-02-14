a846fed3caa527cddc594bdafff27fe3b636a30e
Francis Boisclair <frb@pixmob.com>
2020-02-13 20:06:11 -0500

    Add basic script to get random symbols

---
+++ Assets/NBack/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PhyGames.NBack
+{
+    public class GameController : MonoBehaviour
+    {
+        public Symbol[] m_Symbols;
+
+        private Symbol m_CurrentSymbol;
+        private Queue m_previousSymbols;
+
+
+        private void Start()
+        {
+            m_previousSymbols = new Queue();
+            m_CurrentSymbol = GetRandomSymbol();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                m_previousSymbols.Enqueue(m_CurrentSymbol);
+                m_CurrentSymbol = GetRandomSymbol();
+            }
+        }
+
+
+        private Symbol GetRandomSymbol()
+        {
+            return m_Symbols[Random.Range(0, m_Symbols.Length)];
+        }
+    }
+}
