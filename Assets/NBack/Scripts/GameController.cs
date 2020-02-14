b0e544d051fb3223b02aa3dc539af2774ddb32b5
Francis Boisclair <frb@pixmob.com>
2020-02-13 20:06:11 -0500

    Add NBack module

---
+++ Assets/NBack/Scripts/GameController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace PhyGames.NBack
+{
+    public class GameController : MonoBehaviour
+    {
+        public int m_BackAmount = 1;
+        public Symbol[] m_Symbols;
+        public Image m_ImageHolder;
+        public GameObject m_ControlsUI;
+
+        private Symbol m_CurrentSymbol;
+        private LinkedList<Symbol> m_SymbolsSequence;
+
+
+        private void Start()
+        {
+            m_ControlsUI.SetActive(false);
+            m_SymbolsSequence = new LinkedList<Symbol>();
+            StartCoroutine("ShowStartingSymbols");
+        }
+
+
+        public void HandleSameSymbolAction()
+        {
+            ValidateSymbol(true);
+            ShowNextSymbol();
+        }
+
+        public void HandleDifferentSymbolAction()
+        {
+            ValidateSymbol(false);
+            ShowNextSymbol();
+        }
+
+        private void ValidateSymbol(bool input)
+        {
+            Symbol symbolToCompare = GetPreviousSymbol();
+            if ((m_CurrentSymbol == symbolToCompare) == input)
+            {
+                Debug.Log("Good");
+            }
+            else
+            {
+                Debug.Log("Bad");
+            }
+        }
+
+        private Symbol GetPreviousSymbol()
+        {
+            LinkedListNode<Symbol> node = m_SymbolsSequence.Last;
+
+            for (int i = 1; i < m_BackAmount; i++)
+            {
+                node = node.Previous;
+            }
+
+            return node.Value;
+        }
+
+
+        private IEnumerator ShowStartingSymbols()
+        {
+            int symbolsShown = 0;
+            while (symbolsShown < m_BackAmount)
+            {
+                ShowNextSymbol();
+                symbolsShown++;
+                yield return new WaitForSeconds(2);
+            }
+
+            m_ControlsUI.SetActive(true);
+            ShowNextSymbol();
+        }
+
+        private void AssignSymbol(Symbol symbol)
+        {
+            m_CurrentSymbol = symbol;
+            m_ImageHolder.sprite = m_CurrentSymbol.image;
+        }
+
+        private void ShowNextSymbol()
+        {
+            if (m_CurrentSymbol) 
+            {
+                m_SymbolsSequence.AddLast(m_CurrentSymbol);
+            }
+
+            AssignSymbol(GetRandomSymbol());
+        }
+
+        private Symbol GetRandomSymbol()
+        {
+            return m_Symbols[Random.Range(0, m_Symbols.Length)];
+        }
+    }
+}
---
+++ Assets/NBack/Scripts/Symbols/Symbol.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PhyGames.NBack
+{
+    [CreateAssetMenu(fileName = "Symbol", menuName = "NBack/Create a new symbol", order = 0)]
+    public class Symbol : ScriptableObject
+    {
+        public string id;
+        public Sprite image;
+    }
+}
