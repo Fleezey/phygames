26ece9b57daa1b0cad19234e7aa6b2968993e5c0
Francis Boisclair <francis.l.boisclair@gmail.com>
2020-04-04 00:44:51 -0400

    Add basic main menu

---
+++ Assets/MainMenu/Scripts/MainMenuUIController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuUIController : MonoBehaviour
+{
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
